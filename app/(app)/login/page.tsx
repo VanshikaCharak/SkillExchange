14b5f1557a6cdf638d793bff6df2e9f4ef834c45
SehajModi <modisehaj@gmail.com>
2026-03-29 10:18:55 +0530

    Update page.tsx

--- app/(app)/login/page.tsx
+++ app/(app)/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { useAuth } from "../../hooks";
+import { useAuth } from "../../api";
 
 export default function LoginPage() {
   const { login, register } = useAuth();
